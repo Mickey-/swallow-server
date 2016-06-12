c40da3f356e20ae656dfbd66c4ce93aea44203ca
半边 <itaofe@gmail.com>
2016-06-12 20:14:18 +0800

    init login logic

--- router.js
+++ router.js
@@ -2,7 +2,8 @@ var controller = require('./controller');
 
 module.exports = function(router) {
   router.get('/', controller.index);
-  router.get('/x', controller.x);
+  router.get('/login', controller.login);
+  router.post('/login', controller.doLogin);
   router.get('/gettoken', controller.getToken)
   router.post('/uploadImg', controller.uploadImg);
 }
