d429830022f17e7d5b08be5373bcde802c34a187
小巡 <yangx@59store.com>
2016-07-13 14:11:01 +0800

    fix bug

--- router.js
+++ router.js
@@ -15,7 +15,6 @@ module.exports = function(router) {
     router.get('/poster', poster.find);             //查询海报信息
     router.get('/poster/me', poster.findOwnerPoster);//查询我的海报信息
     router.get('/poster/detail', poster.detail);    //获取海报详情
-    router.post('/poster/publish', poster.publish); //发布海报
     router.put('/poster', poster.update);           //更新海报信息
     router.delete('/poster', poster.del);           //删除海报信息
 
